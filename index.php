0168505cffcfc2ca887c1bca3a4732471035e324
Fedor Ezeev <fezeev@gmail.com>
2012-02-25 14:25:35 +0300

    добавлено опциональное включение отладки

--- index.php
+++ index.php
@@ -12,6 +12,14 @@ else
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
+// remove the following lines when in production mode
+if (isset($_GET['testing']) && $_GET['testing']==1){
+        defined('YII_DEBUG') or define('YII_DEBUG',true);
+}
+else {
+	defined('YII_DEBUG') or define('YII_DEBUG',false);
+}
+
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
