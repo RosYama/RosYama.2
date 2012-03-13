13cb32c348642554a275665b4f09e4622db0e403
Fedor Ezeev <fezeev@gmail.com>
2012-03-13 08:10:33 +0400

    http://dev.rosyama.ru/bugs/show_bug.cgi?id=49

--- protected/views/holes/view.php
+++ protected/views/holes/view.php
@@ -113,7 +113,6 @@ $this->widget('application.extensions.fancybox.EFancyBox', array(
 							</div>
 							<div class="rc">
 								Также можно отправить:<br />
-								<span><ins>&mdash;</ins>с&nbsp;<a href="/about/112/">сайта 112.ru</a></span>
 								<span><ins>&mdash;</ins>с&nbsp;официального сайта <a href="http://www.gibdd.ru/letter" target="_blank">ГИБДД&nbsp;МВД&nbsp;России</a></span>
 							</div>
 						</div>
@@ -143,7 +142,6 @@ $this->widget('application.extensions.fancybox.EFancyBox', array(
 							</div>
 							<div class="rc">
 								Также можно отправить:<br />
-								<span><ins>&mdash;</ins>с&nbsp;<a href="/about/112/">сайта 112.ru</a></span>
 								<span><ins>&mdash;</ins>с&nbsp;официального сайта <a href="http://www.gibdd.ru/letter" target="_blank">ГИБДД&nbsp;МВД&nbsp;России</a></span>
 							</div>						
 						<? endif; ?>	
