caa5abde8cccb448a775e6e1b5a98ace6829812c
Fedor Ezeev <fezeev@gmail.com>
2012-04-23 11:10:22 +0400

    Вынес часть текста из-под ссылки

--- protected/views/ugmail/user2user.php
+++ protected/views/ugmail/user2user.php
@@ -2,6 +2,6 @@
 <p>Пользователь <?php echo CHtml::link($fromuser->Fullname, array('/profile/view', 'id'=>$fromuser->id));?>, отправил вам сообщение:</p>
 <p><?php echo nl2br($model->body); ?></p>
 <br/>
-<p><?php echo CHtml::link('Написать ответ на это письмо можно на страничке '.$this->createUrl('/profile/view', Array('id'=>$fromuser->id)), array('/profile/view', 'id'=>$fromuser->id));?></p>
+<p>Написать ответ на это письмо можно на страничке <?php echo CHtml::link($this->createUrl('/profile/view', Array('id'=>$fromuser->id)), array('/profile/view', 'id'=>$fromuser->id));?></p>
 -----<br/>
 <?php echo CHtml::link('РосЯма', array('/holes/index'));?>
