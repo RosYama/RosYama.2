eb597fd40c20093c10078d521ada26d9f1a107ff
Fedor Ezeev <fezeev@gmail.com>
2012-05-17 23:21:35 +0400

    перенёс форму по добавлению ЯДа чуть пониже

--- protected/views/site/pages/donations.php
+++ protected/views/site/pages/donations.php
@@ -11,8 +11,6 @@ $this->layout='//layouts/header_default_without_add';
 <p><b>Деньги в поддержку РосЯмы можно перечислять сюда:</b></p>
 
 <p><b>Яндекс.Деньги</b> <a href="http://yaudit.org/yaudit/41001550415485">кошелёк № 41001550415485</a></p>
-<!-- Яндекс-форма по перечислению денег -->
-<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/donate.xml?uid=41001550415485&amp;default-sum=300&amp;targets=%d0%bd%d0%b0+%d1%80%d0%b0%d0%b1%d0%be%d1%82%d1%83+%d0%bf%d1%80%d0%be%d0%b5%d0%ba%d1%82%d0%b0&amp;project-name=%d0%a0%d0%be%d1%81%d0%af%d0%bc%d0%b0&amp;project-site=http%3a%2f%2frosyama.ru&amp;button-text=02&amp;hint=" width="450" height="106"></iframe>
 <!-- Кнопка Я-Аудита -->
 <p><button onclick="window.open('http://yaudit.org/41001550415485','newwindow','toolbar=0')">Посмотреть историю пожертвований</button></p>
 
@@ -26,4 +24,8 @@ $this->layout='//layouts/header_default_without_add';
 № счета получателя: 40817810606050014327<br>
 Назначение платежа: Дарение.
 </p>
+
+<!-- Яндекс-форма по перечислению денег -->
+<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/donate.xml?uid=41001550415485&amp;default-sum=300&amp;targets=%d0%bd%d0%b0+%d1%80%d0%b0%d0%b1%d0%be%d1%82%d1%83+%d0%bf%d1%80%d0%be%d0%b5%d0%ba%d1%82%d0%b0&amp;project-name=%d0%a0%d0%be%d1%81%d0%af%d0%bc%d0%b0&amp;project-site=http%3a%2f%2frosyama.ru&amp;button-text=02&amp;hint=" width="450" height="106"></iframe>
+
 </div>
