232f6d046814d62cb33abf37d94f796bd464bc90
Fedor Ezeev <fezeev@gmail.com>
2012-05-26 20:14:35 +0400

    добавил информацию о кошельке QIWI

--- protected/views/site/pages/donations.php
+++ protected/views/site/pages/donations.php
@@ -10,6 +10,8 @@ $this->layout='//layouts/header_default_without_add';
 <div class="rCol">
 <p><b>Деньги в поддержку РосЯмы можно перечислять сюда:</b></p>
 
+<p><b>QIWI</b> - 9161775694</p>
+
 <p><b>Яндекс.Деньги</b> <a href="http://yaudit.org/yaudit/41001550415485">кошелёк № 41001550415485</a></p>
 <!-- Кнопка Я-Аудита -->
 <p><button onclick="window.open('http://yaudit.org/41001550415485','newwindow','toolbar=0')">Посмотреть историю пожертвований</button></p>
