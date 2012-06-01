394eb233ba06b7dfc01198a0afb8d831f8777ea5
vlasovilya <arbprint@mail.ru>
2012-06-01 18:44:51 +0400

    Восстановил права на загрузку точек на карту.

--- protected/controllers/HolesController.php
+++ protected/controllers/HolesController.php
@@ -27,7 +27,7 @@ class HolesController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view', 'findSubject', 'findCity', 'map', 'flushcashe'),
+				'actions'=>array('index','view', 'findSubject', 'findCity', 'map', 'flushcashe', 'ajaxMap'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
