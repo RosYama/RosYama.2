942784e19399ca322e594f9a2eddcc54b3a3e03c
vlasovilya <arbprint@mail.ru>
2012-06-01 18:35:43 +0400

    Убрал вывод архивных ям из общей статистики

--- protected/widgets/collection/collectionWidget.php
+++ protected/widgets/collection/collectionWidget.php
@@ -19,20 +19,20 @@ class collectionWidget extends CWidget {
 
 
         public function run() {
-        $model=Holes::model()->count(Array('condition'=>'PREMODERATED=1 AND archive=0'));
+        $model=Holes::model()->count(Array('condition'=>'PREMODERATED=1'));
         $all=$model;
-        $model=Holes::model()->count(Array('condition'=>'PREMODERATED=1 AND archive=0 AND STATE="inprogress"'));
+        $model=Holes::model()->count(Array('condition'=>'PREMODERATED=1 AND STATE="inprogress"'));
         $ingibdd=$model;
-        $model=Holes::model()->count(Array('condition'=>'PREMODERATED=1 AND archive=0 AND STATE="fixed"'));
+        $model=Holes::model()->count(Array('condition'=>'PREMODERATED=1 AND STATE="fixed"'));
         $fixed=$model;
-        $model=Holes::model()->count(Array('condition'=>'archive=1'));
-        $archive=$model;  
+        //$model=Holes::model()->count(Array('condition'=>'archive=1'));
+        //$archive=$model;  
             $this->registerCoreScripts();
                 $this->render($this->itemview, Array(
                 'all'=>Y::declOfNum($all, array('дефект', 'дефекта', 'дефектов')),
 				'ingibdd'=>$ingibdd,
 				'fixed'=>$fixed,
-				'archive'=>$archive,
+				//'archive'=>$archive,
                 ));
         }
 }
--- protected/widgets/collection/views/default.php
+++ protected/widgets/collection/views/default.php
@@ -1,3 +1,3 @@
 <p class="collection">
-	Наша коллекция насчитывает<br /><strong><a href="/map/"><?php echo $all; ?></a> / <a href="/map/?STATE[2]=inprogress"><?php echo $ingibdd; ?>&nbsp; в гибдд</a> / <a href="/map/?STATE[3]=fixed"><?php echo $fixed; ?>&nbsp;исправлено</a> / <?php echo CHtml::link($archive.'&nbsp;в архиве', Array('/holes/index','Holes[archive]'=>1)); ?></strong>
+	Наша коллекция насчитывает<br /><strong><a href="/map/"><?php echo $all; ?></a> / <a href="/map/?STATE[2]=inprogress"><?php echo $ingibdd; ?>&nbsp; в гибдд</a> / <a href="/map/?STATE[3]=fixed"><?php echo $fixed; ?>&nbsp;исправлено</a> <?php /* / <?php echo CHtml::link($archive.'&nbsp;в архиве', Array('/holes/index','Holes[archive]'=>1)); */ ?></strong>
 </p>
