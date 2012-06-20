e2d6923032118859c6e34b987caa0c6996c9abd0
Fedor Ezeev <fezeev@gmail.com>
2012-06-20 13:39:36 +0400

    bug 138

--- protected/views/ugmail/newComment.php
+++ protected/views/ugmail/newComment.php
@@ -1,5 +1,5 @@
 <h2>Здравствуйте, <?php echo $user->fullName; ?></h2>
-<p>Пользователь <?php echo CHtml::link($comment->user->Fullname, array('http://'.$_SERVER['HTTP_HOST'].'/profile/view', 'id'=>$comment->user->id));?>, оставил комментарий к вашей яме:</p>
+<p>Пользователь <?php echo CHtml::link($comment->user->Fullname, 'http://'.$_SERVER['HTTP_HOST'].'/profile/view/id/'.$comment->user->id);?>, оставил комментарий к вашей яме:</p>
 <p><?php echo nl2br(CHtml::encode($comment->comment_text)); ?></p>
 <p>Ссылка: <?php echo CHtml::link('http://'.$_SERVER['HTTP_HOST'].$comment->pageUrl, 'http://'.$_SERVER['HTTP_HOST'].$comment->pageUrl); ?></p>
 <br/><br/><br/>
