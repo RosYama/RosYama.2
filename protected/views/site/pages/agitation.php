d5ce088f724975712d2ba13489c0b92b2e0b85da
Fedor Ezeev <fezeev@gmail.com>
2012-10-31 22:22:03 +0400

    проверка таблички с файликами

--- protected/views/site/pages/agitation.php
+++ protected/views/site/pages/agitation.php
@@ -1,13 +1,75 @@
 <?
 $this->pageTitle=Yii::app()->name . ' :: Печатная агитация для распространения';
 ?>
+
 <h1>Вы можете помочь проекту, распространяя печатные материалы</h1>
 
-<p>
-<ol>
-	<li>Текстовая листовка</li>
-	<li>Листовка-картинка цветная, чёрно-белая</li>
-	<li></li>
-</ol>
+<h3>Листовки</h3>
+<table>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+</table>
 
-</p>
+<h3>Наклейки</h3>
+
+<table>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+<tr>
+	<td></td>
+	<td></td>
+</tr>
+</table>
+
+<h3>Логотип</h3>
+<table>
+<tr>
+	<td><a href="/files/logo.cdr">Логотип</a></td>
+	<td><img src="/files/logo-small.png"></td>
+</tr>
+</table>
+
