765b32e9df2956c552351e3d2998d2b3d77dd92f
Dmitry Nikiforov <axshavan@yandex.ru>
2013-07-06 13:20:17 +0400

    Упс :)

--- protected/migrations/m130629_103731_bug_223.php
+++ protected/migrations/m130629_103731_bug_223.php
@@ -4,19 +4,19 @@ class m130629_103731_bug_223 extends CDbMigration
 {
 	public function up()
 	{
-		/*$DB  =& Yii::app()->db;
+		$DB  =& Yii::app()->db;
 		$sql = $DB->createCommand("insert into ".$DB->tablePrefix."usergroups_social_services
 			(`name`, `service_name`) values ('forum', 'forum')");
-		$sql->execute();*/
+		$sql->execute();
 		m130629_103731_bug_223::clearCache();
 	}
 
 	public function down()
 	{
-		/*$DB  =& Yii::app()->db;
+		$DB  =& Yii::app()->db;
 		$sql = $DB->createCommand("delete from ".$DB->tablePrefix."usergroups_social_services
 			where `name` = 'forum' and `service_name` = 'forum'");
-		$sql->execute();*/
+		$sql->execute();
 		m130629_103731_bug_223::clearCache();
 	}
 
