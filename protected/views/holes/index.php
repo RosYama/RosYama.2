62ca4bea487d9c6bb2b529a7a27faddf8870f419
Fedor Ezeev <fezeev@gmail.com>
2013-12-03 11:48:06 +0400

    удалил номер своих ЯДенег из баннера

--- protected/views/holes/index.php
+++ protected/views/holes/index.php
@@ -251,8 +251,7 @@ EOD
 <table style="background-color: rgb(241, 109, 126)"> 
   <tbody> 
     <tr><td style="padding-top: 10px; padding-right: 10px; padding-bottom: 10px; padding-left: 10px"> <b> 
-      <p>Сбор средств на развитие РосЯмы:</p>
-        <p>Яндекс-деньги: 41001550415485</p>
+      <p>РосЯма существует на ваши пожертвования:</p>
         <p><a href="/page/donations/">Подробнее</a></p>
       </b>
     </td></tr>
